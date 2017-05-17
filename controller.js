90d3fe09fe67cd93bafdd26419b08afc6a9c8815
sargent88 <steen.sargent@gmail.com>
2017-05-17 13:30:08 -0600

    bluth x2

--- controller.js
+++ controller.js
@@ -1,10 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
-  $scope.getUsers();
-
+    $scope.getUsers = function() {
+        var promise = mainService.getUsers();
+        promise.then(function(users) {
+            $scope.users = users.data;
+        });
+    }
+    $scope.getUsers();
 });
